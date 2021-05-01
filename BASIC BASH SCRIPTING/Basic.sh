6973a3f83fe2e13666ef26807937ecacf85e90f8
Hafiz Muhammad Attaullah <attaullahshafiq10@gmail.com>
2021-05-01 21:43:39 +0500

    Update Basic.sh

--- BASIC BASH SCRIPTING/Basic.sh
+++ BASIC BASH SCRIPTING/Basic.sh
@@ -1,1 +1,26 @@
 
+nano example.sh
+
+
+
+#!/bin/bash
+
+
+#!/bin/bash
+
+# This is a comment and will not be interpreted
+
+
+#!/bin/bash
+
+# This is a comment and will not be interpreted
+
+
+echo "This is a line of text that will be displayed"
+echo
+echo "This line will also be displayed"
+
+chmod 755 example.sh
+
+
+./example.sh
